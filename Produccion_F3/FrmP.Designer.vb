60c3df3c9364acd7b55334e8ffa1df639e0a3cdb
crissdua <crissdua@hotmail.com>
2018-01-28 13:09:46 -0600

    Funcionando falta emision y recibo

---
+++ Produccion_F3/FrmP.Designer.vb
@@ -0,0 +1,350 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmP
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.DGV3 = New System.Windows.Forms.DataGridView()
+        Me.Ordenes = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.DGV2 = New System.Windows.Forms.DataGridView()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtOrder = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.DGV = New System.Windows.Forms.DataGridView()
+        CType(Me.DGV3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DGV2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(454, 124)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(26, 128)
+        Me.Button5.TabIndex = 92
+        Me.Button5.Text = "<"
+        Me.Button5.UseVisualStyleBackColor = True
+        Me.Button5.Visible = False
+        '
+        'DGV3
+        '
+        Me.DGV3.AllowUserToAddRows = False
+        Me.DGV3.AllowUserToDeleteRows = False
+        Me.DGV3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV3.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Ordenes})
+        Me.DGV3.Location = New System.Drawing.Point(486, 6)
+        Me.DGV3.Name = "DGV3"
+        Me.DGV3.ReadOnly = True
+        Me.DGV3.Size = New System.Drawing.Size(118, 246)
+        Me.DGV3.TabIndex = 91
+        Me.DGV3.Visible = False
+        '
+        'Ordenes
+        '
+        Me.Ordenes.HeaderText = "Ordenes"
+        Me.Ordenes.Name = "Ordenes"
+        Me.Ordenes.ReadOnly = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(454, 6)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(26, 121)
+        Me.Button4.TabIndex = 90
+        Me.Button4.Text = ">"
+        Me.Button4.UseVisualStyleBackColor = True
+        Me.Button4.Visible = False
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"AM", "PM", "Corto"})
+        Me.ComboBox1.Location = New System.Drawing.Point(324, 6)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 89
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(282, 11)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(38, 13)
+        Me.Label8.TabIndex = 88
+        Me.Label8.Text = "Turno:"
+        '
+        'DGV2
+        '
+        Me.DGV2.AllowUserToAddRows = False
+        Me.DGV2.AllowUserToDeleteRows = False
+        Me.DGV2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV2.Location = New System.Drawing.Point(136, 124)
+        Me.DGV2.Name = "DGV2"
+        Me.DGV2.ReadOnly = True
+        Me.DGV2.Size = New System.Drawing.Size(312, 128)
+        Me.DGV2.TabIndex = 87
+        Me.DGV2.Visible = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(3, 65)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(90, 13)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Cant. Planificada:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(98, 65)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Label7"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(3, 43)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(84, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Desc. Producto:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(98, 43)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Label5"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Cod. Producto:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(98, 24)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(120, 17)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(50, 17)
+        Me.ToolStripStatusLabel2.Text = "Usuario:"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel2, Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 289)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(619, 22)
+        Me.StatusStrip1.TabIndex = 86
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "No. Doc:"
+        '
+        'txtOrder
+        '
+        Me.txtOrder.AutoSize = True
+        Me.txtOrder.Location = New System.Drawing.Point(98, 5)
+        Me.txtOrder.Name = "txtOrder"
+        Me.txtOrder.Size = New System.Drawing.Size(39, 13)
+        Me.txtOrder.TabIndex = 0
+        Me.txtOrder.Text = "Label1"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(12, 11)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(63, 13)
+        Me.Label13.TabIndex = 85
+        Me.Label13.Text = "Buscar Item"
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.txtOrder)
+        Me.Panel1.Location = New System.Drawing.Point(136, 29)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(312, 89)
+        Me.Panel1.TabIndex = 84
+        Me.Panel1.Visible = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(343, 258)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(89, 23)
+        Me.Button3.TabIndex = 83
+        Me.Button3.Text = "Salir"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(248, 258)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(89, 23)
+        Me.Button2.TabIndex = 82
+        Me.Button2.Text = "Cancelar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(153, 258)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(89, 23)
+        Me.Button1.TabIndex = 81
+        Me.Button1.Text = "Aceptar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 29)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(118, 20)
+        Me.TextBox2.TabIndex = 80
+        '
+        'DGV
+        '
+        Me.DGV.AllowUserToAddRows = False
+        Me.DGV.AllowUserToDeleteRows = False
+        Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV.Location = New System.Drawing.Point(12, 52)
+        Me.DGV.Name = "DGV"
+        Me.DGV.ReadOnly = True
+        Me.DGV.Size = New System.Drawing.Size(118, 200)
+        Me.DGV.TabIndex = 79
+        '
+        'FrmP
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(619, 311)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.DGV3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.DGV2)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.DGV)
+        Me.Name = "FrmP"
+        Me.Text = "FrmP"
+        CType(Me.DGV3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DGV2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button5 As Button
+    Friend WithEvents DGV3 As DataGridView
+    Friend WithEvents Ordenes As DataGridViewTextBoxColumn
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents DGV2 As DataGridView
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtOrder As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents DGV As DataGridView
+End Class
---
+++ Produccion_F3/FrmP.vb
@@ -0,0 +1,284 @@
+﻿Imports System.Windows.Forms
+Imports System.IO
+Imports System.Data.SqlClient
+Imports CrystalDecisions.Shared
+Imports CrystalDecisions.CrystalReports.Engine
+Imports CrystalDecisions.Windows.Forms
+Imports CrystalDecisions.ReportSource
+Public Class FrmP
+#Region "Variables"
+    Public con As New Conexion
+    Public itemcode As String
+    Dim valBarCode As String
+    Dim oCompany As SAPbobsCOM.Company
+    Dim connectionString As String = Conexion.ObtenerConexion.ConnectionString
+    Public Ready As Boolean
+    Private Const CP_NOCLOSE_BUTTON As Integer = &H200
+    Public Shared SQL_Conexion As SqlConnection = New SqlConnection()
+    Public Shared ba As New List(Of String)
+    Public Shared quantity As New List(Of Integer)
+    Public Shared itemsimp As New List(Of String)
+    Public Shared quantityimp As New List(Of Integer)
+    Public Shared anchoimp As New List(Of Double)
+    Public Shared wo As SAPbobsCOM.ProductionOrders
+#End Region
+    Protected Overloads Overrides ReadOnly Property CreateParams() As CreateParams
+        Get
+            Dim myCp As CreateParams = MyBase.CreateParams
+            myCp.ClassStyle = myCp.ClassStyle Or CP_NOCLOSE_BUTTON
+            Return myCp
+        End Get
+    End Property
+
+    Public Sub New(ByVal user As String)
+        MyBase.New()
+        InitializeComponent()
+        '  Note which form has called this one
+        ToolStripStatusLabel1.Text = user
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        Dim SQL_da As SqlDataAdapter = New SqlDataAdapter("SELECT T0.DocNum FROM OWOR T0 where T0.Type = 'P' and T0.Status = 'P' and T0.DocNum LIKE '" + TextBox2.Text + "%' ORDER BY T0.DocNum", con.ObtenerConexion())
+        Dim DT_dat As System.Data.DataTable = New System.Data.DataTable()
+        SQL_da.Fill(DT_dat)
+        DGV.DataSource = DT_dat
+        con.ObtenerConexion.Close()
+    End Sub
+
+    Private Sub FrmP_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox2.Select()
+        ComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
+        cargaORDER()
+    End Sub
+
+    Public Function cargaORDER()
+        Dim SQL_da As SqlDataAdapter = New SqlDataAdapter("SELECT T0.DocNum FROM OWOR T0 where T0.Type = 'P' and T0.Status = 'P'", con.ObtenerConexion())
+        Dim DT_dat As System.Data.DataTable = New System.Data.DataTable()
+        SQL_da.Fill(DT_dat)
+        DGV.DataSource = DT_dat
+        con.ObtenerConexion.Close()
+    End Function
+
+    Private Sub DGV_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGV.CellContentClick
+        txtOrder.Text = DGV(0, DGV.CurrentCell.RowIndex).Value.ToString()
+        Panel1.Visible = True
+        DGV2.Visible = True
+        Button4.Visible = True
+        Button5.Visible = True
+        DGV3.Visible = True
+        Dim SQL_da As SqlDataAdapter = New SqlDataAdapter("SELECT T0.ItemCode, T2.ItemName, T0.PlannedQty FROM OWOR T0 inner join OITM T2 on T0.ItemCode = T2.ItemCode where T0.DocNum= '" + txtOrder.Text + "'", con.ObtenerConexion())
+        Dim DT_dat As System.Data.DataTable = New System.Data.DataTable()
+        SQL_da.Fill(DT_dat)
+        Label3.Text = DT_dat.Rows(0).Item("ItemCode").ToString
+        Label5.Text = DT_dat.Rows(0).Item("ItemName").ToString
+        Label7.Text = DT_dat.Rows(0).Item("PlannedQty").ToString & " TM"
+        con.ObtenerConexion.Close()
+
+        Dim turnoAM_I As DateTime = CType("6:00:00 AM", DateTime)
+        Dim turnoAM_F As DateTime = CType("6:00:00 PM", DateTime)
+
+        Dim result As Integer = 0
+        Dim result2 As Integer = 0
+        result = DateTime.Compare(turnoAM_I, TimeOfDay.ToShortTimeString)
+        result2 = DateTime.Compare(turnoAM_F, TimeOfDay.ToShortTimeString)
+        If result = -1 Then
+            If result2 = 1 Then
+                ComboBox1.Text = "AM"
+            Else
+                ComboBox1.Text = "PM"
+            End If
+        End If
+    End Sub
+
+    Private Sub txtOrder_TextChanged(sender As Object, e As EventArgs) Handles txtOrder.TextChanged
+        Dim SQL_da As SqlDataAdapter = New SqlDataAdapter("SELECT T0.ItemCode, T0.BaseQty, T0.U_ancho,T0.U_tiras,T0.U_peso,isnull(T0.LineNum,0) FROM WOR1 T0 where T0.[DocEntry] like '" + txtOrder.Text + "%'", con.ObtenerConexion())
+        Dim DT_dat As System.Data.DataTable = New System.Data.DataTable()
+        SQL_da.Fill(DT_dat)
+        DGV2.DataSource = DT_dat
+        con.ObtenerConexion.Close()
+    End Sub
+
+    Private Sub Update()
+        Dim oReturn As Integer = -1
+        Dim oError As Integer = 0
+        Dim errMsg As String = ""
+        Dim sql As String
+        Dim oRecordSet As SAPbobsCOM.Recordset
+        Dim objectCode As Integer
+
+        Dim itemname As String
+        Dim pesoreal As Double
+        Dim postdate As String
+
+        Try
+            Dim result As Integer = MessageBox.Show("Desea Ingresar la Orden?", "Atencion", MessageBoxButtons.YesNoCancel)
+            If result = DialogResult.Cancel Then
+                MessageBox.Show("Cancelado")
+            ElseIf result = DialogResult.No Then
+                MessageBox.Show("No se realizara la orden")
+            ElseIf result = DialogResult.Yes Then
+                Dim cont As Integer = 0
+                For Each row As DataGridViewRow In DGV3.Rows
+
+                    If con.Connected = True Then
+                        sql = ("select T0.DocEntry, T0.ItemCode, T1.itemName, T0.PlannedQty,T0.PostDate,T0.DueDate from OWOR T0 inner join OITM T1 on T0.itemcode = t1.itemcode where T0.Docnum = '" + DGV3.Rows(cont).Cells.Item(0).Value.ToString + "'")
+                        oRecordSet = con.oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+                        oRecordSet.DoQuery(sql)
+                        If oRecordSet.RecordCount > 0 Then
+                            objectCode = oRecordSet.Fields.Item(0).Value
+                            itemcode = oRecordSet.Fields.Item(1).Value
+                            itemname = oRecordSet.Fields.Item(2).Value
+                            pesoreal = oRecordSet.Fields.Item(3).Value
+                            postdate = oRecordSet.Fields.Item(4).Value
+                        End If
+
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(oRecordSet)
+                        oRecordSet = Nothing
+                        GC.Collect()
+                        'Dim items As String
+                        'For Each row As DataGridViewRow In DGV2.Rows
+                        '    items += row.Cells(0).Value.ToString + "|"
+                        'Next
+                        'imprime(items.TrimEnd("|"), itemname, "AnchoTira", pesoreal, items.TrimEnd("|"), "heat", "coil", docdate)
+
+
+
+
+                        wo = con.oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oProductionOrders)
+                        wo.GetByKey(objectCode)
+                        wo.ProductionOrderStatus = SAPbobsCOM.BoProductionOrderStatusEnum.boposReleased
+                        'wo.Update()
+                        oReturn = wo.Update()
+                        If oReturn <> 0 Then
+                            MessageBox.Show(con.oCompany.GetLastErrorDescription)
+                        Else
+
+                            Dim items As String
+                            sql = ("select T0.U_lotes, T0.U_tiras, T0.U_ancho from wor1 T0 where T0.DocEntry = '" + objectCode.ToString + "'")
+                            'sql = ("SELECT DocEntry FROM WOR1 where DocEntry = " + objectCode + "")
+                            oRecordSet = con.oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+                            oRecordSet.DoQuery(sql)
+                            Dim vueltas As Integer = Convert.ToInt32(oRecordSet.RecordCount)
+                            Do While Not oRecordSet.EoF
+                                itemsimp.Add(oRecordSet.Fields.Item(0).Value)
+                                quantityimp.Add(oRecordSet.Fields.Item(1).Value)
+                                anchoimp.Add(oRecordSet.Fields.Item(2).Value)
+                                oRecordSet.MoveNext()
+                            Loop
+
+                            System.Runtime.InteropServices.Marshal.ReleaseComObject(oRecordSet)
+                            oRecordSet = Nothing
+                            GC.Collect()
+
+                            Dim conts As Integer
+
+                            Do While vueltas > 0
+                                conts = vueltas - 1
+                                Dim cont1 As Integer
+                                cont1 = 0
+                                Do While cont1 < Convert.ToInt32(quantityimp.Item(conts))
+                                    imprime(itemsimp.Item(conts), itemname, anchoimp.Item(conts), pesoreal, itemsimp.Item(conts), "heat", "coil", postdate, ComboBox1.Text)
+                                    cont1 += cont1 + 1
+                                Loop
+                                vueltas = vueltas - 1
+                            Loop
+
+
+                            itemcode = String.Empty
+                            items = String.Empty
+                            Panel1.Visible = False
+                            DGV2.Visible = True
+                            cargaORDER()
+                            DGV2.DataSource = Nothing
+                            DGV3.Rows.Clear()
+                            MessageBox.Show("Finalizado con Exito")
+                        End If
+
+                    End If
+                    cont = cont + 1
+                Next
+                con.oCompany.Disconnect()
+                con.oCompany = Nothing
+            End If
+        Catch ex As Exception
+            MsgBox("Error: " + ex.Message.ToString)
+            con.oCompany.Disconnect()
+            con.oCompany = Nothing
+        End Try
+    End Sub
+    Private Function FormatBarCode(code As String)
+        Dim barcode As String = String.Empty
+        barcode = String.Format("*{0}*", code)
+        Return barcode
+    End Function
+    Private Sub imprime(bat As String, desc As String, anch As String, pes As String, bob As String, het As String, coi As String, ordr As String, turnos As String)
+        Dim Report1 As New CrystalDecisions.CrystalReports.Engine.ReportDocument()
+        Report1.PrintOptions.PaperOrientation = PaperOrientation.Portrait
+        Report1.Load(Application.StartupPath + "\Report\Informe.rpt", CrystalDecisions.Shared.OpenReportMethod.OpenReportByDefault.OpenReportByDefault)
+        Report1.SetParameterValue("CodBatch", itemcode)
+        'Report1.SetParameterValue("CodBatch", txtBarcode.Text)
+        Report1.SetParameterValue("descripcion", desc)
+        Report1.SetParameterValue("anchotira", anch)
+        Report1.SetParameterValue("pesoreal", pes)
+        Report1.SetParameterValue("bobina", bob)
+        Report1.SetParameterValue("heat", het)
+        Report1.SetParameterValue("coil", coi)
+        Report1.SetParameterValue("ordencorte", ordr)
+        Report1.SetParameterValue("fechacorte", Now.ToShortDateString)
+        Report1.SetParameterValue("turno", turnos)
+        'CrystalReportViewer1.ReportSource = Report1
+        Report1.PrintToPrinter(1, False, 0, 0)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If con.Connected Then
+                Update()
+            Else
+                con.MakeConnectionSAP()
+                If con.Connected Then
+                    Update()
+                Else
+                    MessageBox.Show("Error de Conexion, intente Nuevamente")
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox("Error: " + ex.Message.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim result As Integer = MessageBox.Show("Desea limpiar el objeto?", "Atencion", MessageBoxButtons.YesNoCancel)
+        If result = DialogResult.Cancel Then
+            MessageBox.Show("Cancelado")
+        ElseIf result = DialogResult.No Then
+            MessageBox.Show("Puede continuar!")
+        ElseIf result = DialogResult.Yes Then
+            TextBox2.Clear()
+            txtOrder.Text = ""
+            wo = Nothing
+            DGV.DataSource = Nothing
+            DGV2.DataSource = Nothing
+            DGV3.Rows.Clear()
+            MessageBox.Show("Inicie un objeto nuevo")
+        End If
+        Panel1.Visible = False
+        DGV2.Visible = False
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Dim existe As Boolean = DGV3.Rows.Cast(Of DataGridViewRow).Any(Function(x) CInt(x.Cells("Ordenes").Value) = DGV(0, DGV.CurrentCell.RowIndex).Value.ToString())
+        If Not existe Then
+            DGV3.Rows.Add(DGV(0, DGV.CurrentCell.RowIndex).Value.ToString())
+        Else
+            MessageBox.Show("Ya ingreso ese numero de orden")
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        DGV3.Rows.Remove(DGV3.CurrentRow)
+    End Sub
+
+
+End Class
